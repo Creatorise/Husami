9d1e9f6107bf1aecb5260f4794fb942987e75258
Creatorise <manager@creatorise.com>
2022-01-30 17:07:42 +0100

    Add logger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const users_router = require('./users/users_router')
 
+const pino = require('pino')({ level: 'warn' })
+const expressPino = require('express-pino-logger')({
+    logger: pino,
+})
 const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.static('client/public'))
 app.use('/api/users', users_router)
+
+process.on('SIGINT', () => {
+    console.log(`process.on ~ SIGINT`, SIGINT)
+    app.close()
+    database.close()
+})
 
 module.exports = app
