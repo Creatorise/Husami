e407ff2ab75f201aad04b071c60f32f6a2edff7e
Creatorise <manager@creatorise.com>
2022-01-31 22:00:24 +0100

    Add response when trying to create invalid house

--- src/houses/houses_controllers.js
+++ src/houses/houses_controllers.js
@@ -1,10 +1,11 @@
-const houses = require('./houses_service')
+// const houses = require('./houses_service')
 
 async function create_house(req, res) {
+    res.status(400).send({ success: false })
     // const { name, email, role } = req.body
     // const house = { name, email, role }
     // const result = await house.create(house)
     // if (!result.success) return res.status(422).send(result)
-    return res.status(201).end()
+    // return res.status(201).end()
 }
 module.exports = { create_house }
--- tests/houses.spec.js
+++ tests/houses.spec.js
@@ -13,4 +13,23 @@ afterAll(async () => {
     await database.close()
 })
 
-describe('Create a new house', () => {})
+const admin_auth_cookie =
+    'auth_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFmNzAxMzVlNDdjYTA3ZDMzMTExZjM3Iiwicm9sZSI6ImFkbWluIn0sImlhdCI6MTY0MzYzNTE2OX0.hxj7ixm9_4sl__Bx41rnyeElW8c1w-TuGjhrrlYcscc'
+
+describe('Create a new house', () => {
+    describe('Invalid house', () => {
+        let response
+        beforeAll(async () => {
+            response = await server
+                .post('/api/houses')
+                .send({})
+                .set('Cookie', admin_auth_cookie)
+        })
+        test('Response status 400', () => {
+            expect(response.status).toBe(400)
+        })
+        test('Response body success false', () => {
+            expect(response.body.success).toBe(false)
+        })
+    })
+})
