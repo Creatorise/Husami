a2f0edd55cbb0f48fd5346ea978f902fd105fe73
Creatorise <manager@creatorise.com>
2022-03-14 09:25:27 +0100

    Add login script

--- client/script.js
+++ client/script.js
@@ -1,5 +1,23 @@
 const login__form = document.querySelector('.login__form');
+console.log(`login__form`, login__form);
 
-login__form.addEventListener('submit', (e) => {
+login__form.addEventListener('submit', async (e) => {
     e.preventDefault();
+
+    const email = e.target.email.value;
+    console.log(`login__form.addEventListener ~ email`, email);
+
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+    });
+    console.log(`login__form.addEventListener ~ response`, response);
+    if (!response.ok) {
+        console.log(`login__form.addEventListener ~ response.status`, response.status);
+        return;
+    }
 });
