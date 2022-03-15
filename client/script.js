465d66b884a9a52e26b1a3b8edb32e5eb931c1e2
Creatorise <manager@creatorise.com>
2022-03-15 16:34:09 +0100

    Add login states

--- client/script.js
+++ client/script.js
@@ -1,10 +1,18 @@
 const login__form = document.querySelector('.login__form');
+const login__waiting = document.querySelector('.login__waiting');
+const login__rejected = document.querySelector('.login__rejected');
+
+const state = {
+    login: 'login',
+};
 
 login__form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const email = e.target.email.value;
     console.log(`login__form.addEventListener ~ email`, email);
+
+    set_login_state('waiting');
 
     const response = await fetch('/api/auth/login', {
         method: 'POST',
@@ -15,7 +23,38 @@ login__form.addEventListener('submit', async (e) => {
         credentials: 'include',
         body: JSON.stringify({ email }),
     });
-    if (!response.ok) {
+    // if (!response.ok) {
+    set_login_state('rejected');
+    // return;
+    // }
+    // set_login_state('waiting');
+});
+
+function set_login_state(new_state) {
+    state.login = new_state;
+
+    react_to_state();
+}
+
+function react_to_state() {
+    if (state.login == 'login') {
+        login__form.style.display = 'block';
+        login__waiting.style.display = 'none';
         return;
     }
-});
+    if (state.login == 'waiting') {
+        login__form.style.display = 'none';
+        login__waiting.style.display = 'block';
+        return;
+    }
+    if (state.login == 'rejected') {
+        login__form.style.display = 'none';
+        login__waiting.style.display = 'none';
+        login__rejected.style.display = 'block';
+    }
+    if (state.login == 'logged_in') {
+        login__form.style.display = 'none';
+        login__waiting.style.display = 'none';
+        return;
+    }
+}
